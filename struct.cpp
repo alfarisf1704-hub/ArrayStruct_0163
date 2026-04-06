745f498d44f7eb6e94794f7172f73d49c951b0a3
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-04-06 15:25:09 +0700

    int main struct

--- struct.cpp
+++ struct.cpp
@@ -7,3 +7,19 @@ struct orang
     string alamat;
     int umur;
 };
+
+int main()
+{
+    orang mhs;
+    cout << "Nama : ";
+    cin >> mhs.nama;
+    cout << "Alamat : ";
+    cin >> mhs.nama;
+    cout << "Umur : ";
+    cin >> mhs.nama;
+    cout << endl;
+    // tampilkan
+    cout << "Nama : " << mhs.nama << endl;
+    cout << "Alamat : " << mhs.alamat << endl;
+    cout << "Umur : " << mhs.umur << endl;
+}

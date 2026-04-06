160d150a7e696ed523a035931e55703cc9115035
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-04-06 15:15:05 +0700

    menambahkan keterangan

--- Array.cpp
+++ Array.cpp
@@ -29,10 +29,12 @@ int main()
     cout << "Data Kelima = " << kamu[4] << endl;
     cout << endl;
 
+    // mengisi dengan menggunakan perulangan for
     for (int i = 0; i <= 4; i++)
     {
         cout << "Data index ke-" << i << " = ";
         cin >> kamu[i];
     }
     cout << endl;
+    // menampilkan dengan menggunakan perulangan for
 }

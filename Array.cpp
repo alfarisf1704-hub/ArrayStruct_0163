9b851ec8be49018ae9438d883263b1f47992c088
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-04-06 15:09:52 +0700

    cout data pertama sampai lima

--- Array.cpp
+++ Array.cpp
@@ -19,4 +19,13 @@ int main()
     cout << "Data index 3 = ";
     cin >> kamu[3];
     cout << "Data index 4 = ";
+    cin >> kamu[4];
+    cout << endl;
+
+    cout << "Data Pertama = " << kamu[0] << endl;
+    cout << "Data Kedua = " << kamu[1] << endl;
+    cout << "Data Ketiga = " << kamu[2] << endl;
+    cout << "Data Keempat = " << kamu[3] << endl;
+    cout << "Data Kelima = " << kamu[4] << endl;
+    cout << endl;
 }
